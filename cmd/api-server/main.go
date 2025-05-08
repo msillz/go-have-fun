d918a4a41c801a414496a129e7d99509eff7c383
michael sills <michaeldsills@gmail.com>
2025-05-08 15:23:49 -0700

    Added a clickhouse connection

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,26 +1,44 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/joho/godotenv"
 	"github.com/msillz/go-have-fun/pkg/morestrings"
 	"rsc.io/quote"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbHost := os.Getenv("CLICKHOUSE_HOST")
+	dbPassword := os.Getenv("CLICKHOUSE_PASSWORD")
 
-	conn, err := clickhouse.Open(&clickhouse.Options{
-		Addr: []string{"localhost:9000"},
+	conn := clickhouse.OpenDB(&clickhouse.Options{
+		Addr:     []string{dbHost}, // 9440 is a secure native TCP port
+		Protocol: clickhouse.Native,
+		TLS:      &tls.Config{}, // enable secure TLS
 		Auth: clickhouse.Auth{
-			Database: "default",
 			Username: "default",
-			Password: "",
+			Password: dbPassword,
 		},
-		Protocol: clickhouse.Native, // or clickhouse.HTTP
 	})
+
+	row := conn.QueryRow("SELECT 1")
+	var col uint8
+	if err := row.Scan(&col); err != nil {
+		fmt.Printf("An error while reading the data: %s", err)
+	} else {
+		fmt.Printf("Result: %d", col)
+	}
 
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 
