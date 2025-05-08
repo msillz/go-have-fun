71211466102a57d4d9ff395d70d7b2a33f9988f0
michael sills <michaeldsills@gmail.com>
2025-05-08 10:57:14 -0700

    first commit

---
+++ Makefile
@@ -0,0 +1,18 @@
+# Variables
+BINARY_NAME=api-server
+CMD_DIR=./cmd/api-server
+
+# Default target: build the binary
+build:
+	go build -o $(BINARY_NAME) $(CMD_DIR)
+
+# Run the application
+run:
+	go run $(CMD_DIR)
+
+# Clean up the binary
+clean:
+	rm -f $(BINARY_NAME)
+
+# Phony targets
+.PHONY: build run clean
---
+++ cmd/api-server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"rsc.io/quote"
+)
+
+func main() {
+
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"message": quote.Go()})
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
